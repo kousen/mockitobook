c91028158f75d7a336ca5a10b363da0522765825
Ken Kousen <ken.kousen@kousenit.com>
2022-03-02 10:24:40 -0500

    Add test of BioService with static mock

--- src/main/java/com/kousenit/wikipedia/BioService.java
+++ src/main/java/com/kousenit/wikipedia/BioService.java
@@ -1,31 +1,18 @@
 package com.kousenit.wikipedia;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BioService {
-    private List<String> pageNames;
+    private final List<String> pageNames;
 
-    public BioService() {
-        pageNames = List.of("Anita Borg", "Ada Lovelace",
-                "Grace Hopper", "Barbara Liskov");
-    }
-
-    public BioService(List<String> pageNames) {
-        this.pageNames = pageNames;
-    }
-
-    public void setPageNames(List<String> pageNames) {
-        this.pageNames = pageNames;
-    }
-
-    public List<String> getPageNames() {
-        return pageNames;
+    public BioService(String... pageNames) {
+        this.pageNames = Arrays.stream(pageNames).toList();
     }
 
     public List<String> getBios() {
         return pageNames.stream()
                 .map(WikiUtil::getWikipediaExtract)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
--- src/test/java/com/kousenit/wikipedia/BioServiceTest.java
+++ src/test/java/com/kousenit/wikipedia/BioServiceTest.java
@@ -1,30 +1,44 @@
 package com.kousenit.wikipedia;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
 
 class BioServiceTest {
-    private final BioService service = new BioService();
-
-    @Test
+    @Test // Integration test
     void checkBios() {
+        BioService service = new BioService("Anita Borg", "Ada Lovelace",
+                "Grace Hopper", "Barbara Liskov");
         List<String> bios = service.getBios();
         assertEquals(4, bios.size());
         bios.forEach(bio -> {
             String[] strings = bio.split(":");
             String[] bioStrings = strings[1].split("\\n");
             System.out.println("Title: " + strings[0]);
-            Arrays.stream(bioStrings).forEach(System.out::println);
+            Arrays.stream(bioStrings)
+                    .forEach(System.out::println);
             System.out.println("-------------------");
         });
     }
 
     @Test
     void testBioServiceWithMocks() {
-
+        BioService service = new BioService("Anita Borg", "Ada Lovelace",
+                "Grace Hopper", "Barbara Liskov");
+        try (MockedStatic<WikiUtil> mocked = mockStatic(WikiUtil.class)) {
+            mocked.when(() -> WikiUtil.getWikipediaExtract(anyString()))
+                    .thenReturn("Borg", "Hopper", "Lovelace", "Liskov");
+            List<String> bios = service.getBios();
+            System.out.println(bios);
+            mocked.verify(() -> WikiUtil.getWikipediaExtract(anyString()),
+                    times(4));
+        }
     }
 }
